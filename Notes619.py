564a8a8387230f1a3f5d6d48a9b36053d28722b7
Katherine Howitt <kghowitt@gmail.com>
2019-06-19 14:43:21 -0400

    Updated code with William to and Notes

---
+++ Notes619.py
@@ -0,0 +1,7 @@
+#run on French CoLAG to test for Pied Piping
+#add in WH movement on the presence of a WH
+#headedness in IP
+#presence of an s is irrelevant for null subject
+
+#qinv - only relevant if question
+
